8f899f80ac35f352d5da94708ea2fa65249d211b
Afraiven <filip.antoniak99@gmail.com>
2023-10-28 15:21:51 +0200

    tempaltki

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('sharefood/', sharefood, name='sharefood'),
+    path('shareitems/', shareitems, name='shareitems'),
+    path('shareskills/', shareskills, name='shareskills'),
+    path('events/', events, name='events'),
+    path('fundacje/', fundacje, name='fundacje'),
+
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -9,3 +9,42 @@ def index(request):
         "lorem": "lorem",
     }
     return HttpResponse(template.render(context, request))
+
+
+def sharefood(request):
+    template = loader.get_template("frontend/sharefood.html")
+    context = {
+        "lorem": "lorem",
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def shareitems(request):
+    template = loader.get_template("frontend/shareitems.html")
+    context = {
+        "lorem": "lorem",
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def shareskills(request):
+    template = loader.get_template("frontend/shareskills.html")
+    context = {
+        "lorem": "lorem",
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def events(request):
+    template = loader.get_template("frontend/events.html")
+    context = {
+        "lorem": "lorem",
+    }
+    return HttpResponse(template.render(context, request))
+
+def fundacje(request):
+    template = loader.get_template("frontend/fundacje.html")
+    context = {
+        "lorem": "lorem",
+    }
+    return HttpResponse(template.render(context, request))
