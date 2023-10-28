1020da64146f408c7ffabff96a113e23346e49ad
Afraiven <filip.antoniak99@gmail.com>
2023-10-28 16:21:23 +0200

    xmisns

--- BestHacks2023/backend/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Organization, Event, Image
-
-admin.site.register(CustomUser)
-admin.site.register(Organization)
-admin.site.register(Event)
-admin.site.register(Image)
