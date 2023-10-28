fea3dd0d1df9b55b7697413094df12a4674603c9
Dawid Błaszczyk <skiperpol1@gmail.com>
2023-10-28 18:48:59 +0200

    views

--- backend/views.py
+++ backend/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
-from backend.forms import FormJedzenie, FormUserRegistration
+from backend.forms import FormJedzenie, FormPrzedmiot, FormUserRegistration, FormUsluga
 from django.template import loader
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import logout
 
-from backend.models import CustomUser, Jedzenie
+from backend.models import CustomUser, Jedzenie, Przedmiot, Usluga
 
 def is_admin(user):
     return user.is_superuser
@@ -41,6 +41,18 @@ def sharefood(request):
 
 
 def shareitems(request):
+    user = request.user
+    rola = user.rola
+    if rola == "POTRZEBUJACY":
+        przedmiot = Przedmiot.objects.all()
+        return render(request, 'frontend/sharefood.html', {'przedmiot': przedmiot, 'rola': rola})
+    elif rola == 'WOLONTARIUSZ':
+        form = FormPrzedmiot()
+        print("111111")
+        return render(request, 'frontend/sharefood.html', {'form': form, 'rola': rola})
+    else:
+        pass
+
     template = loader.get_template("frontend/shareitems.html")
     context = {
         "lorem": "lorem",
@@ -49,6 +61,17 @@ def shareitems(request):
 
 
 def shareskills(request):
+    user = request.user
+    rola = user.rola
+    if rola == "POTRZEBUJACY":
+        usluga = Usluga.objects.all()
+        return render(request, 'frontend/sharefood.html', {'usluga': usluga, 'rola': rola})
+    elif rola == 'WOLONTARIUSZ':
+        form = FormUsluga()
+        return render(request, 'frontend/sharefood.html', {'form': form, 'rola': rola})
+    else:
+        pass
+
     template = loader.get_template("frontend/shareskills.html")
     context = {
         "lorem": "lorem",
@@ -139,3 +162,57 @@ def createUser(request):
     print("END")
     return HttpResponse('Stworzono nowego użytkownika')
 
+
+
+# AJAX
+
+
+@ensure_csrf_cookie
+def AjaxCreateFood(request):
+    superusers = CustomUser.objects.filter(is_superuser=True)
+    if request.user.is_superuser:
+        form = AdminOrderForm(request.POST, request.FILES)
+        users = request.POST.getlist('uzytkownik', None)
+    else:
+        form = OrderForm(request.POST, request.FILES)
+    files = request.FILES.getlist('files[]', None)
+    order = form.save(commit=False)
+    order.save()
+    zalaczniki_publiczne = []
+    if request.user.is_superuser:
+        order.uzytkownik.add(*users)
+        order.save()
+    else:
+        order.uzytkownik.add(request.user)
+        order.save()
+    for file in files:
+        zalacznik = OrderZalacznikPubliczne.objects.create(zalacznikPubliczne=file)
+        zalaczniki_publiczne.append(zalacznik)
+    uwagaPubliczna = UwagiPubliczne.objects.create(uzytkownik=request.user, order=order, trescPubliczne="Stworzono zlecenie")
+    uwagaPubliczna.save()
+    uwagaPubliczna.zalacznikiPubliczne.add(*zalaczniki_publiczne)
+    if request.user.is_superuser: 
+        for user in users:
+            existing_user = get_object_or_404(CustomUser, id=user)
+            createStatus = OrderStatus.objects.create(uzytkownik=existing_user, order=order, status="DO_WYKONANIA")
+            createStatus.save()  
+    else:
+        for superUser in superusers:
+            createStatus = OrderStatus.objects.create(uzytkownik=superUser, order=order, status="DO_WYKONANIA")
+            createStatus.save()
+    createStatus = OrderStatus.objects.create(uzytkownik=request.user, order=order, status="DO_WYKONANIA")
+    createStatus.save()
+    addAllUsers(order.id,request.user)
+    users_list = order.uzytkownik.all()
+    users_email_list=[]
+    for user in users_list:
+        users_email_list.append(user.email)
+    if(order.nr_zlecenia!=""):
+        message = "Stworzono nowe zlecenie o numerze "+order.nr_zlecenia+"."
+    else:
+        message = "Stworzono nowe zlecenie."
+
+    # t = threading.Thread(target=sending, args=(request, users_email_list, 'Stworzono zlecenie', message))
+    # t.start()
+    sending(request, users_email_list, 'Stworzono zlecenie', message)
+    return JsonResponse({'msg':'<span style="color: green;">File successfully uploaded</span>'})
